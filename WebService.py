b2e0fe24b451d7ac128aa5b5805d9266dd4b01cd
Asterik Rafael Winoto <asterikrafael@gmail.com>
2020-06-17 11:28:19 +0700

    Update WebService.py

--- WebService.py
+++ WebService.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def presensi():
     tic = time.process_time()
-    path='/webService/Code'
+    path='/Code'
     names=[d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
     i=0
     while i<len(names):
